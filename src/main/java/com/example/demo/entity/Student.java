a4f43220eb03e7e21bb3b994ca3e472628ae62b4
Messyo <messyoroboto@gmail.com>
2021-08-02 18:48:57 -0300

    Developed entity Student

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "Student")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private Integer age;
+
+}
